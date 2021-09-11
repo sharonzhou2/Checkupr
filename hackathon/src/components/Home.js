5270a7e14112b98b0fb96358554fe990b8f622d2
Sharon Zhou <sharonzhou@phoebes-mbp-2.home>
2021-09-11 15:38:05 +1000

    Updated with routes

---
+++ hackathon/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Checkupr</h1>
+            <h2>Healthcare for everyone</h2>
+        </div>
+    )
+}
+
+export default Home
